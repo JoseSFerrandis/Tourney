d2ff831bddb14c2f08d7cdfdf808de8fbfcdac35
josfermon8 <josfermon8@alu.edu.gva.es>
2026-01-30 19:11:05 +0100

    Changes: Modificar el Login y el Dashboard + navigation

--- app/src/main/java/com/example/tourney/DashboardFragment.kt
+++ app/src/main/java/com/example/tourney/DashboardFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.tourney.databinding.FragmentDashboardBinding
-import com.example.tourney.databinding.FragmentSecondBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
