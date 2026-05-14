ae1edbee0c7464d8a6402da10fa6e74d2901fb3e
juaculmor <juandanielculianez@gmail.com>
2026-05-14 19:56:11 +0200

    Fix: Forzada la orientación de la pantalla a vertical y configurado el manejo manual de cambios de configuración en la actividad principal

--- app/src/main/java/com/example/tourney/fragments/AccountManagement.kt
+++ app/src/main/java/com/example/tourney/fragments/AccountManagement.kt
@@ -1,15 +1,20 @@
 package com.example.tourney.fragments
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.tourney.R
 import com.example.tourney.databinding.FragmentAccountManagementBinding
 import com.example.tourney.entities.User
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class AccountManagement : Fragment() {
 
@@ -71,6 +76,33 @@ class AccountManagement : Fragment() {
         } else {
             binding.ivProfile.setImageResource(R.drawable.ic_user_pfp1)
         }
+    }
+
+    private fun showCustomHomeDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_insert_current_password, null)
+        val dialog = MaterialAlertDialogBuilder(requireContext())
+            .setView(dialogView)
+            .create()
+
+        // Ajustar fondo transparente para que se vean los bordes redondeados del CardView
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val btnAccept = dialogView.findViewById<Button>(R.id.btnAccept)
+        val btnCancel = dialogView.findViewById<Button>(R.id.btnCancel)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        btnCreate.setOnClickListener {
+            navController.navigate(R.id.action_HomeFragment_to_CreateTournamentFragment)
+            dialog.dismiss()
+        }
+
+        btnJoin.setOnClickListener {
+            navController.navigate(R.id.action_HomeFragment_to_JoinTournamentFragment)
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     override fun onDestroyView() {
